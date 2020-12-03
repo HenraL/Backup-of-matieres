2ee19d19291598892b5d56d40ed88dddc0e8d80e
HenraL <37299738+HenraL@users.noreply.github.com>
2020-12-03 08:25:02 +0100

    Updated and created a few files

--- files/Matières/NSI/T1/.py/Time logger/Time_loger.py
+++ files/Matières/NSI/T1/.py/Time logger/Time_loger.py
@@ -5,7 +5,7 @@ def write(r,d):
  f.close()
 date=datetime.datetime.now()
 f=open("Time1.log","a")
-f.write("Date=\"{}/{}/{}\" Start Time=\"{}/{}/{}\"\nCreated By Henry Letellier\n".format(date.day,date.month,date.year,date.hour,date.minute,date.second))
+f.write("Date=\"{}/{}/{}\" Start Time=\"{}/{}/{}\"\nCreated By Henry Letellier\n".format(date.day,date.month,date.year,date.second,date.minute,date.hour))
 f.close()
 i=0
 r=0
