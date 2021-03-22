b9bdea518f9c7bb3b17ac52b23bd3c66d8c3af56
HenraL <37299738+HenraL@users.noreply.github.com>
2021-03-22 22:09:34 +0100

    uploaded the daily files

--- files/Matières/NSI/T1/cours/Les graphs/001_Les_graphs_prog_1.py
+++ files/Matières/NSI/T1/cours/Les graphs/001_Les_graphs_prog_1.py
@@ -18,36 +18,77 @@ class noeud:
         print("self.pointeur_voisin2={}".format(self.pointeur_voisin2))
         print("self.pointeur_voisin3={}".format(self.pointeur_voisin3))
         print("self.pointeur_voisin4={}".format(self.pointeur_voisin4))
-        print()
+        #print()
 
 
 # matrice d'ajacement orienté a->b b->c b->d
 
-#      a b c d
+#------>
+#sens de lecture
+# X    a b c d     Y
 # M =[[0,1,1,0],  #a
     # [0,0,1,1],  #b
     # [0,0,0,0],  #c
     # [0,0,0,0]]  #d
 
 # je créé mes 4 noeuds non liés pour le moment
-
-noeud_A=noeud("A",None,1,1,None)
-noeud_B=noeud("B",None,None,1,1)
-noeud_C=noeud("C",None,None,None,None)
-noeud_D=noeud("D",None,None,None,None)
-
+#X                        Y
+noeud_A=noeud("A",0,0,0,0)
+noeud_B=noeud("B",1,0,0,0)
+noeud_C=noeud("C",1,1,0,0)
+noeud_D=noeud("D",0,1,0,0)
 # liaison manuelle:
 # (visualisez chaque nouvelle ligne sous python tutor)
-noeud_A.pointeur_voisin2=noeud_B
-noeud_A.pointeur_voisin3=noeud_C
-noeud_B.pointeur_voisin3=noeud_C
-noeud_B.pointeur_voisin4=noeud_D
+noeud_A.pointeur_voisin2=noeud_B.pointeur_voisin2
+noeud_A.pointeur_voisin3=noeud_C.pointeur_voisin3
+noeud_B.pointeur_voisin3=noeud_C.pointeur_voisin3
+noeud_B.pointeur_voisin4=noeud_D.pointeur_voisin1
 
 
-print(noeud_A.pointeur_voisin2)
+noeud_AA=noeud("A","A1","A2","A3","A4")
+noeud_BB=noeud("B","B1","B2","B3","B4")
+noeud_CC=noeud("C","C1","C2","C3","C4")
+noeud_DD=noeud("D","D1","D2","D3","D4")
+# liaison manuelle:
+# (visualisez chaque nouvelle ligne sous python tutor)
+noeud_AA.pointeur_voisin2=noeud_BB.pointeur_voisin2
+noeud_AA.pointeur_voisin3=noeud_CC.pointeur_voisin3
+noeud_BB.pointeur_voisin3=noeud_CC.pointeur_voisin3
+noeud_BB.pointeur_voisin4=noeud_DD.pointeur_voisin1
+"""noeud_A=noeud("A",11,12,13,14)
+noeud_B=noeud("B",21,22,23,24)
+noeud_C=noeud("C",31,32,33,34)
+noeud_D=noeud("D",41,42,43,44)"""
+
+print("XY")
+print("  A B C D")
+print("A",noeud_A.pointeur_voisin1,noeud_B.pointeur_voisin1,noeud_C.pointeur_voisin1,noeud_D.pointeur_voisin1)
+print("B",noeud_A.pointeur_voisin2,noeud_B.pointeur_voisin2,noeud_C.pointeur_voisin2,noeud_D.pointeur_voisin2)
+print("C",noeud_A.pointeur_voisin3,noeud_B.pointeur_voisin3,noeud_C.pointeur_voisin3,noeud_D.pointeur_voisin3)
+print("D",noeud_A.pointeur_voisin4,noeud_B.pointeur_voisin4,noeud_C.pointeur_voisin4,noeud_D.pointeur_voisin4)
+print("XY")
+print("  A B C D")
+print("A",noeud_AA.pointeur_voisin1,noeud_BB.pointeur_voisin1,noeud_CC.pointeur_voisin1,noeud_DD.pointeur_voisin1)
+print("B",noeud_AA.pointeur_voisin2,noeud_BB.pointeur_voisin2,noeud_CC.pointeur_voisin2,noeud_DD.pointeur_voisin2)
+print("C",noeud_AA.pointeur_voisin3,noeud_BB.pointeur_voisin3,noeud_CC.pointeur_voisin3,noeud_DD.pointeur_voisin3)
+print("D",noeud_AA.pointeur_voisin4,noeud_BB.pointeur_voisin4,noeud_CC.pointeur_voisin4,noeud_DD.pointeur_voisin4)
+print("YX")
+print("  A B C D")
+print("A",noeud_A.pointeur_voisin1,noeud_A.pointeur_voisin2,noeud_A.pointeur_voisin3,noeud_A.pointeur_voisin4)
+print("B",noeud_B.pointeur_voisin1,noeud_B.pointeur_voisin2,noeud_B.pointeur_voisin3,noeud_B.pointeur_voisin4)
+print("C",noeud_C.pointeur_voisin1,noeud_C.pointeur_voisin2,noeud_C.pointeur_voisin3,noeud_C.pointeur_voisin4)
+print("D",noeud_D.pointeur_voisin1,noeud_D.pointeur_voisin2,noeud_D.pointeur_voisin3,noeud_D.pointeur_voisin4)
+print("YX")
+print("  A B C D")
+print("A",noeud_AA.pointeur_voisin1,noeud_AA.pointeur_voisin2,noeud_AA.pointeur_voisin3,noeud_AA.pointeur_voisin4)
+print("B",noeud_BB.pointeur_voisin1,noeud_BB.pointeur_voisin2,noeud_BB.pointeur_voisin3,noeud_BB.pointeur_voisin4)
+print("C",noeud_CC.pointeur_voisin1,noeud_CC.pointeur_voisin2,noeud_CC.pointeur_voisin3,noeud_CC.pointeur_voisin4)
+print("D",noeud_DD.pointeur_voisin1,noeud_DD.pointeur_voisin2,noeud_DD.pointeur_voisin3,noeud_DD.pointeur_voisin4)
+
+"""print(noeud_A.pointeur_voisin2)
 print(noeud_A.pointeur_voisin3)
 print(noeud_B.pointeur_voisin3)
 print(noeud_B.pointeur_voisin4)
 
-#
+#"""
 
