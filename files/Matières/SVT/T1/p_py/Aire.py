44b6fc937dde8ee46a4a1b78b4b15c02289704ad
Gitlab_test_project <letellier.henry@gmail.com>
2021-05-16 21:08:03 +0200

    uploaded the daily files

---
+++ files/Matières/SVT/T1/p_py/Aire.py
@@ -0,0 +1,11 @@
+from math import pi
+class aire:
+    def _init_(self):
+        self.PI=pi
+    def carré(c):
+        print(f"{c}*{c}={c*c}²")
+        return c*c
+    def cercle(self,R):
+        print("pi*(R)²=r².pi")
+        print(f"{self.pi}*({R})²={self.pi*(R)**2)")
+        return self.pi*(R)**2
