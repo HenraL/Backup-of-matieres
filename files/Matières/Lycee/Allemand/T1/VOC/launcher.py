b27fe4c8ba29ea7505fef4a42be9e8552c154ade
Gitlab_test_project <letellier.henry@gmail.com>
2021-05-22 13:34:50 +0200

    reorganized the files

---
+++ files/Matières/Lycee/Allemand/T1/VOC/launcher.py
@@ -0,0 +1,12 @@
+from Verbe_sorter_and_checker import *
+"""entspanen = se détendre
+einladen = inviter
+ein Luftballon = un ballon
+fêter = feiern
+Schach = échecs
+Staubsauger = aspirateur
+Blume = fleurs
+nebenan = à coté
+brav = sage
+Silvester = le jour de l'an"""
+
